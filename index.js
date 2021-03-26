00327f3157d5a2ccd7b6cb13219bd6d1b10f6ee8
Glenn Wiley <glenn.wiley@wwt.com>
2021-03-26 15:51:37 -0500

    update port - GW

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const port = 3000;
 
 const init = async () => {
 
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || port,
         host: 'localhost'
     });
 
